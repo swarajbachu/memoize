4585fe237e7ba4af38b0c4d3de68b1a70d475e7c
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-09-07 17:57:04 +0530

    fix format

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,17 +1,17 @@
-import "@bacons/text-decoder/install";
+import '@bacons/text-decoder/install'
 
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "nativewind";
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { useColorScheme } from 'nativewind'
 
-import { TRPCProvider } from "~/utils/api";
+import { TRPCProvider } from '~/utils/api'
 
-import "../styles.css";
+import '../styles.css'
 
 // This is the main layout of the app
 // It wraps your pages with the providers they need
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
+  const { colorScheme } = useColorScheme()
   return (
     <TRPCProvider>
       {/*
@@ -21,14 +21,14 @@ export default function RootLayout() {
       <Stack
         screenOptions={{
           headerStyle: {
-            backgroundColor: "#f472b6",
+            backgroundColor: '#f472b6',
           },
           contentStyle: {
-            backgroundColor: colorScheme === "dark" ? "#09090B" : "#FFFFFF",
+            backgroundColor: colorScheme === 'dark' ? '#09090B' : '#FFFFFF',
           },
         }}
       />
       <StatusBar />
     </TRPCProvider>
-  );
+  )
 }
