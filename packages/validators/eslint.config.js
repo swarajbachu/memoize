ed308d97bdbcbff08c76915d92b43d61658b42e0
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-09-07 17:54:26 +0530

    biome added

--- packages/validators/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "@memoize/eslint-config/base";
-
-/** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: ["dist/**"],
-  },
-  ...baseConfig,
-];
