49030f7c8fa648f9ff6ba754e24bff460d6a1b9e
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-09-14 02:39:14 +0530

    removed little trpc, and removed cache: no-store, expecting it to break, but lets see

--- apps/application/src/app/page.tsx
+++ apps/application/src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function HomePage() {
   const test = await res.json()
 
   return (
-    <HydrateClient>
+    <>
       <main className="container h-screen py-16">
         <div className="flex flex-col items-center justify-center gap-4">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
@@ -22,6 +22,6 @@ export default async function HomePage() {
           <div className="w-full max-w-2xl overflow-y-scroll">cool</div>
         </div>
       </main>
-    </HydrateClient>
+    </>
   )
 }
--- apps/application/src/trpc/react.tsx
+++ apps/application/src/trpc/react.tsx
@@ -28,39 +28,9 @@ export const api = createTRPCReact<AppRouter>()
 export function TRPCReactProvider(props: { children: React.ReactNode }) {
   const queryClient = getQueryClient()
 
-  const [trpcClient] = useState(() =>
-    api.createClient({
-      links: [
-        loggerLink({
-          enabled: (op) =>
-            env.NODE_ENV === 'development' ||
-            (op.direction === 'down' && op.result instanceof Error),
-        }),
-        unstable_httpBatchStreamLink({
-          transformer: SuperJSON,
-          url: getBaseUrl() + '/api/trpc',
-          headers() {
-            const headers = new Headers()
-            headers.set('x-trpc-source', 'nextjs-react')
-            return headers
-          },
-        }),
-      ],
-    }),
-  )
-
   return (
     <QueryClientProvider client={queryClient}>
-      <api.Provider client={trpcClient} queryClient={queryClient}>
-        {props.children}
-      </api.Provider>
+      {props.children}
     </QueryClientProvider>
   )
-}
-
-const getBaseUrl = () => {
-  if (typeof window !== 'undefined') return window.location.origin
-  if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`
-  // eslint-disable-next-line no-restricted-properties
-  return `http://localhost:${process.env.PORT ?? 3000}`
 }
