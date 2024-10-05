9c75f906822f08514085dd316bb2d7133b540de0
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-10-05 22:27:29 +0530

    fixing build issue (runtime on edge)

--- apps/application/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ apps/application/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,8 @@
 import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
+export const runtime = "edge";
+
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 h-screen">
